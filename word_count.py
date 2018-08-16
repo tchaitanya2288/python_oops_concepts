5b80e295f05840f4e5b947be34fd052c55167dff
tchaitanya2288 <tchaitanya.2288@gmail.com>
2018-08-16 17:29:17 -0400

    char_occurences

--- word_count.py
+++ word_count.py
@@ -1,8 +1,15 @@
-str = "This is an awesome occassion.This has never happened before"
+str = "This is an awesome occasion. This has never happened before"
 
 char_occurences = {}
 
 for char in str:
-    char_occurences[char] = char_occurences.get(char,0)+1
+    char_occurences[char] = char_occurences.get(char, 0)+1
 
 print(char_occurences)
+
+word_occurences = {}
+
+for word in str.split():
+    word_occurences[word] = word_occurences.get(word, 0) + 1
+
+print(word_occurences)
